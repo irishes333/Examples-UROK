518a86b37eb1dd88261209f3ec8fc0fa6b98ad58
Irina <DoronchenkovaIA@sibintek.ru>
2022-08-05 10:24:23 +0300

    dobavila ashu

---
+++ Example001_HelloConsole/Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользовавтеля");
+string username=Console.ReadLine();
+
+if(username.ToLower()=="маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+     Console.Write("ПРивет,");
+     Console.WriteLine(username);
+}  
